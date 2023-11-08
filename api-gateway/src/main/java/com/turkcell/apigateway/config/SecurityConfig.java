825ed46ff666683c09cbdbe12e7893031d461e65
emrealkan884 <Hasan.alkan884@gmail.com>
2023-11-08 20:32:19 +0300

    Dockerize v1

--- api-gateway/src/main/java/com/turkcell/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/turkcell/apigateway/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+// Keycloak i dahil etmek icin.
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
